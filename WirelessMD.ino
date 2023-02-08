340080e7367f26657f9bacc79280c9b991314510
sai.aravind <sai.aravind@unboxrobotics.com>
2023-02-08 22:38:33 +0530

    v0.1 with ESP as STA mode

--- WirelessMD.ino
+++ WirelessMD.ino
@@ -1,30 +1,16 @@
 #include <ESP8266WebServer.h>
 #include <FS.h>
 #include <ESP8266WiFi.h>
-
-#include <ESP8266mDNS.h>
 #include "config.h"
 
-
-IPAddress local_IP(192, 168, 4, 22);
-IPAddress gateway(192, 168, 4, 9);
-IPAddress subnet(255, 255, 255, 0);
-
-
+//Define the pins
 int STBY = 12; //standby
 
 ESP8266WebServer server (80);
 
-struct wifiConfig {
-  String ssid;
-  String password;
-};
-
-
 //This function takes the parameters passed in the URL(the x and y coordinates of the joystick)
 //and sets the motor speed based on those parameters.
-void handleJSData() 
-{
+void handleJSData() {
   boolean yDir;
   int x = server.arg(0).toInt() * 10;
   int y = server.arg(1).toInt() * 10;
@@ -48,18 +34,16 @@ void handleJSData()
   //just change the 2 digitalWrite lines for both motors:
   //!ydir would become ydir, and ydir would become !ydir
 
-  /*Serial.print("x=");
-    Serial.print(aSpeed);
-    Serial.print("y=");
-    Serial.println(bSpeed);
-    */
 
-  digitalWrite(STBY, HIGH);
+  Serial.print("x=");
+  Serial.print(aSpeed);
+  Serial.print("y=");
+  Serial.println(bSpeed);
+
   //MotorA
   digitalWrite(AIN1, !yDir);
   digitalWrite(AIN2, yDir);
   analogWrite(PWMA, aSpeed);
-  
   //MotorB
   digitalWrite(BIN1, !yDir);
   digitalWrite(BIN2, yDir);
@@ -69,25 +53,7 @@ void handleJSData()
   server.send(200, "text/plain", "");
 }
 
-struct wifiConfig get_SAPMODE(String filepath)
-{ wifiConfig ap;
-  //Serial.println(filepath);
-  File file = SPIFFS.open(filepath, "r");
-  ap.ssid = file.readStringUntil('\n');
-  ap.password = file.readStringUntil('\n');
-  file.close();
-  return ap;
-}
-
-void reset()
-{
-  Serial.println("Reset pressed");
-  server.send(200, "text/plain", "");
-  ESP.restart();
-
-}
-
-void initGpio() 
+void initGpio()
 {
   pinMode(STBY, OUTPUT);
 
@@ -100,94 +66,61 @@ void initGpio()
   pinMode(BIN2, OUTPUT);
 }
 
-void beginWebserver() 
+void beginWebserver()
 {
 
-  server.serveStatic("/", SPIFFS, "/home.html");
+  //server.serveStatic("/", SPIFFS, "/home.html");
+  //server.serveStatic("/sap/", SPIFFS, "/sap.html");
+
   //call handleJSData function when this URL is accessed by the js in the html file
-  server.on("/home.html", reset);
+  //server.on("/home.html", reset_controller);
+  //server.on("/action_page", getSapdata);
 
 
-
-  server.serveStatic("/control/", SPIFFS, "/joystick.html");
-  server.serveStatic("/control/virtualjoystick.js", SPIFFS, "/virtualjoystick.js");
+  //server.serveStatic("/control/", SPIFFS, "/joystick.html");
+  //server.serveStatic("/control/virtualjoystick.js", SPIFFS, "/virtualjoystick.js");
   //call handleJSData function when this URL is accessed by the js in the html file
-  server.on("/control/jsData.html", handleJSData);
-  server.begin();
-
-}
-
-void setup()
-{ 
-  struct wifiConfig apSettings;
-
-  String ssid = "ESP8266";
-  String password = "123446576543";
-  Serial.begin(115200);
-  delay(2000);
-
-  if (!SPIFFS.begin()) {
-    Serial.println("SPIFFS Mount failed");
-  }
-  else 
-  {
-    Serial.println("SPIFFS Mount succesfull");
-  }
-
-  File ap = SPIFFS.open(APfile, "r");
-  if (!ap) {
-    Serial.println("failed to open SoftAP settings defaulting ssid and password");
-    apSettings.ssid = "Aravind";
-    apSettings.password = "helloboys1";
-  }
-  else {
-    apSettings = get_SAPMODE(APfile);
-    Serial.println("SPIFFS Mount succesfull");
-    delay(2000);
-  }
-
-  Serial.print("SSID:");
-  Serial.println(apSettings.ssid);
-  Serial.print("PASSWORD:");
-  Serial.println(apSettings.password);
-
-
-
-
-  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-  WiFi.softAP(apSettings.ssid, apSettings.password);
-  Serial.print("Setting soft-AP ... ");
-  Serial.print("Soft-AP IP address = ");
-  Serial.println(WiFi.softAPIP());
-
-
-  /*
-    WiFi.begin(apSettings.ssid, apSettings.password);     //Connect to your WiFi router
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }*/
-
-
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
-
-  //set the pins as outputs
-
-  // Debug console
-  //initialize SPIFFS to be able to serve up the static HTML files.
-
-  //  Serial.println("WiFi connected");
-  //  Serial.println("IP address: ");
-  //  Serial.println(WiFi.localIP());
+  //server.on("/control/jsData.html", handleJSData)
 
 
   //set the static pages on SPIFFS for the html and js
+  server.serveStatic("/", SPIFFS, "/joystick.html");
+  server.serveStatic("/virtualjoystick.js", SPIFFS, "/virtualjoystick.js");
+  //call handleJSData function when this URL is accessed by the js in the html file
+  server.on("/jsData.html", handleJSData);
+  server.begin();
+}
 
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  //set the pins as outputs
+  
+  // Debug console
+  //initialize SPIFFS to be able to serve up the static HTML files.
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS Mount failed");
+  }
+  else {
+    Serial.println("SPIFFS Mount succesfull");
+  }
+  
   initGpio();
   beginWebserver();
-
-
 }
 
 void loop()
